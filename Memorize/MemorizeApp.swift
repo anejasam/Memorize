5d9ea128ca082959ac8159e443055cae6676c415
Sidak Aneja <sidak_aneja@sfu.ca>
2023-06-09 14:45:11 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sidak Aneja on 2023-06-09.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
